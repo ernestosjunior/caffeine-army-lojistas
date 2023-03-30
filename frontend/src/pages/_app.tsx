8d40f58fcaa182d62fba3eade8e49fccda9ecfa4
Ernesto Junior <ernesto.sjunior@hotmail.com>
2023-03-30 13:43:16 -0300

    feat: frontend add progressbar

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "styled-components";
+import NextNProgress from "nextjs-progressbar";
 
 import { AuthProvider } from "@/contexts/auth";
 import { ResetStyle } from "@/styles/base";
@@ -8,12 +9,13 @@ import { defaultTheme } from "@/styles/themes";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <ThemeProvider theme={defaultTheme}>
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-        <ResetStyle />
-        <Toaster position="top-right" />
-      </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <AuthProvider>
+        <NextNProgress color={defaultTheme.colors.primary} height={6} />
+        <Component {...pageProps} />
+      </AuthProvider>
+      <ResetStyle />
+      <Toaster position="top-right" />
+    </ThemeProvider>
   );
 }
